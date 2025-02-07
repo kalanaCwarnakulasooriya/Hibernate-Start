70e3e696691743feabe26e2ecc29a98fd72de943
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-02-07 13:00:05 +0530

    Hibernate 1st Day Hibernate.cfg.xml and FactoryConfiguration class 12.59

--- src/main/java/config/FactoryConfiguration.java
+++ src/main/java/config/FactoryConfiguration.java
@@ -1,4 +1,27 @@
 package config;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+    private SessionFactory sessionFactory;
+
+    private FactoryConfiguration() {
+       Configuration configure =  new Configuration().configure();
+       configure.addAnnotatedClass(entity.Customer.class);
+        sessionFactory = configure.buildSessionFactory();
+    }
+
+    public static FactoryConfiguration getInstance(){
+        if (factoryConfiguration == null) {
+            factoryConfiguration = new FactoryConfiguration();
+        }
+        return factoryConfiguration;
+    }
+
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
 }
